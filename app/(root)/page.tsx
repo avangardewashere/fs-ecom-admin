793c4e0549af75aeb967e3d6fd5c89e2b193f376
avangardewashere <avelpanaligan@gmail.com>
2025-01-02 15:01:41 +0800

    add/shadcn

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return <div className="p-3">
+
+    <span><Button>Meow</Button></span>
+  </div>;
+}
