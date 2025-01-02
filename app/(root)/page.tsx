854c1ce5e949000dbfdaff6be6d020e1dc6a05dd
avangardewashere <avelpanaligan@gmail.com>
2025-01-02 19:33:38 +0800

    setup/useStoremodal-insetup-page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,16 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { UserButton } from "@clerk/nextjs";
- 
+import { useEffect } from "react";
 
 export default function SetupPage() {
-  return <div className="p-3">
-
-    <span><Button>Meow</Button>
-    <UserButton/>
-
-    {/* <Modal title="test" description="sa"isOpen onClose={()=>{}}>
-      Children
-    </Modal> */}
-    </span>  
-  </div>;
+  const { isOpen, onOpen } = useStoreModal();
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+  return <div className="p-3">Root Page</div>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,27 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ModalProvider } from "@/providers/modal-provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Admin Dashboard',
-  description: 'Admin',
-}
+  title: "Admin Dashboard",
+  description: "Admin",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}>
+          <ModalProvider /> {children}
+        </body>
+      </html>
     </ClerkProvider>
-  )
+  );
 }
