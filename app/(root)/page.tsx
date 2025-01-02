562b82ef135abef989099e740e7809a206a41038
avangardewashere <avelpanaligan@gmail.com>
2025-01-02 18:26:52 +0800

    add/modal-and-install-zustand

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,18 @@
+"use client"
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Modal } from "@/components/ui/modal";
+import { UserButton } from "@clerk/nextjs";
+ 
 
-export default function Home() {
+export default function SetupPage() {
   return <div className="p-3">
 
-    <span><Button>Meow</Button></span>
+    <span><Button>Meow</Button>
+    <UserButton/>
+
+    {/* <Modal title="test" description="sa"isOpen onClose={()=>{}}>
+      Children
+    </Modal> */}
+    </span>  
   </div>;
 }
