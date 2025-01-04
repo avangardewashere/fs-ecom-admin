b31078ba40a290051b0cc176347cb7959357747a
avangardewashere <avelpanaligan@gmail.com>
2025-01-04 23:37:48 +0800

    update/delete-route-handler

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -47,32 +47,28 @@ export async function DELETE(
     try {
       const { userId } = await auth();
   
-      const body = await req.json();
+     
   
-      const { name } = await body();
+   
       if (!userId) {
         return new NextResponse("Unauthorized", { status: 401 });
       }
   
-      if (!name) {
-        return new NextResponse("Name is required", { status: 400 });
-      }
-  
-      if (!params) {
+      if (!params.storeId) {
         return new NextResponse("Store Id is required", { status: 400 });
       }
   
-      const store = await prismadb.store.updateMany({
+      const store = await prismadb.store.deleteMany({
         where: {
           id: params.storeId,
           userId: userId,
         },
-        data: { name: name },
+      
       });
   
       return NextResponse.json(store);
     } catch (error) {
-      console.log("[STORE_PATCH]", error);
+      console.log("[STORE_DELETE]", error);
     }
     return new NextResponse("Internal error", { status: 500 });
   }
