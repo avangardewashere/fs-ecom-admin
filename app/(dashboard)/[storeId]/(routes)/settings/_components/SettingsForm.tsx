bf08f3d6cddfd5e3be3db13fd8c2aa34246571dc
avangardewashere <avelpanaligan@gmail.com>
2025-01-04 23:50:36 +0800

    add/onsubmit-form

--- app/(dashboard)/[storeId]/(routes)/settings/_components/SettingsForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/_components/SettingsForm.tsx
@@ -13,9 +13,12 @@ import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Store } from "@prisma/client";
+import axios from "axios";
 import { Trash } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { z } from "zod";
 
 interface SettingsFormProps {
@@ -30,15 +33,24 @@ type SettingsFormValues = z.infer<typeof formSchema>;
 
 const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   const [open, setOpen] = useState(false);
-  const [loading, setIsLoading] = useState(false);
-
+  const [loading, setLoading] = useState(false);
+  const params = useParams();
+  const router = useRouter();
   const form = useForm<SettingsFormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData,
   });
 
   const onSubmit = async (data: SettingsFormValues) => {
-    console.log(data);
+    try {
+      setLoading(true);
+      await axios.patch(`/api/stores/${params.storeId}`, data);
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setLoading(true);
+    }
   };
 
   return (
@@ -50,7 +62,9 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
           variant={"destructive"}
           size="icon"
           disabled={loading}
-          onClick={() => {setOpen(true)}}
+          onClick={() => {
+            setOpen(true);
+          }}
         >
           <Trash className="h-4 w-4" />
         </Button>
