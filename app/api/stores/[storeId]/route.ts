71a8c7d88a48942481fb9be7224754bc40151352
avangardewashere <avelpanaligan@gmail.com>
2025-01-04 18:03:02 +0800

    add/route-handler-delete

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -2,7 +2,7 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function Pathc(
+export async function PATCH(
   req: Request,
   { params }: { params: { storeId: string } }
 ) {
@@ -38,3 +38,42 @@ export async function Pathc(
   }
   return new NextResponse("Internal error", { status: 500 });
 }
+
+
+export async function DELETE(
+    req: Request,
+    { params }: { params: { storeId: string } }
+  ) {
+    try {
+      const { userId } = await auth();
+  
+      const body = await req.json();
+  
+      const { name } = await body();
+      if (!userId) {
+        return new NextResponse("Unauthorized", { status: 401 });
+      }
+  
+      if (!name) {
+        return new NextResponse("Name is required", { status: 400 });
+      }
+  
+      if (!params) {
+        return new NextResponse("Store Id is required", { status: 400 });
+      }
+  
+      const store = await prismadb.store.updateMany({
+        where: {
+          id: params.storeId,
+          userId: userId,
+        },
+        data: { name: name },
+      });
+  
+      return NextResponse.json(store);
+    } catch (error) {
+      console.log("[STORE_PATCH]", error);
+    }
+    return new NextResponse("Internal error", { status: 500 });
+  }
+  
