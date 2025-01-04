461ac40edc8bf747fe5a56416cf1bc16433fb39d
avangardewashere <avelpanaligan@gmail.com>
2025-01-04 23:58:22 +0800

    create/alert-modal-component

---
+++ components/ui/modal/alert-modal.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+interface AlertModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  loading:boolean;
+}
+
+export const AlertModal:React.FC<AlertModalProps> = ({isOpen,onClose,onConfirm,loading}) => {
+    
+    
+    return(
+
+)};
