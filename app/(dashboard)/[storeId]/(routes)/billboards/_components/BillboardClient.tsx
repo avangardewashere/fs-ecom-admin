2aa506b38cca428dd50b7e3ce6a8cc31b30cdcb0
avangardewashere <avelpanaligan@gmail.com>
2025-01-05 20:06:46 +0800

    add/single-billboard-page

--- app/(dashboard)/[storeId]/(routes)/billboards/_components/BillboardClient.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/_components/BillboardClient.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import React from "react";
 
 const BillboardClient = () => {
+  const router = useRouter();
+  const params = useParams();
   return (
-    <div>
-      <span>BillboardClient</span>
-    </div>
-  )
-}
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title="Billboards (0)"
+          description="Manage billboards for your store"
+        />
+        <Button
+          onClick={() => {
+            router.push(`/${params.storeId}/billboards/new`);
+          }}
+        >
+          <Plus className="mr-2 h-4 w-4">Add new</Plus>
+        </Button>
+        Billboard client
+      </div>
+    </>
+  );
+};
 
-export default BillboardClient
+export default BillboardClient;
