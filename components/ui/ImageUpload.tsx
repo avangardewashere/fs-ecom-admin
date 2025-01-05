9b4f87344ef34ba6fa6b9912efa7d7a9dbb879c8
avangardewashere <avelpanaligan@gmail.com>
2025-01-06 02:59:07 +0800

    add/imageUpload-and-cloudinary-dependency

---
+++ components/ui/ImageUpload.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+interface ImageUploadProps {
+  disabled?: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[];
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  disabled,
+  onChange,
+  onRemove,
+  value,
+}) => {
+  return <div>Image Uploadi</div>;
+};
+
+export default ImageUpload;
