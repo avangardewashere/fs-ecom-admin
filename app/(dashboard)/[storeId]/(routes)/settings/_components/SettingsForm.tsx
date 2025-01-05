6ad5d76e0bebf724a3c22a18cfdb3047dc771972
avangardewashere <avelpanaligan@gmail.com>
2025-01-05 15:46:47 +0800

    add/prisma-model-billboad

--- app/(dashboard)/[storeId]/(routes)/settings/_components/SettingsForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/_components/SettingsForm.tsx
@@ -13,6 +13,7 @@ import Heading from "@/components/ui/heading";
 import { Input } from "@/components/ui/input";
 import { AlertModal } from "@/components/ui/modal/alert-modal";
 import { Separator } from "@/components/ui/separator";
+import { useOrigin } from "@/hooks/use-origin";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Store } from "@prisma/client";
 import axios from "axios";
@@ -38,6 +39,7 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   const [loading, setLoading] = useState(false);
   const params = useParams();
   const router = useRouter();
+  const origin = useOrigin();
   const form = useForm<SettingsFormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData,
