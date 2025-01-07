f288067e5ee117c1cb69a983555f71eba802f681
avangardewashere <avelpanaligan@gmail.com>
2025-01-08 01:05:07 +0800

    add/sizes-model-sizes-route-sizes-compoents

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/[billboardId]/_components/BillboardForm.tsx
@@ -0,0 +1,195 @@
+"use client";
+
+import ApiAlert from "@/components/ui/ApiAlert";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import Heading from "@/components/ui/heading";
+import ImageUpload from "@/components/ui/ImageUpload";
+import { Input } from "@/components/ui/input";
+import { AlertModal } from "@/components/ui/modal/alert-modal";
+import { Separator } from "@/components/ui/separator";
+import { useOrigin } from "@/hooks/use-origin";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { BillBoard } from "@prisma/client";
+import axios from "axios";
+import { Trash } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { z } from "zod";
+
+const formSchema = z.object({
+  label: z.string().min(1),
+  imageUrl: z.string().min(1),
+});
+
+interface BillboardFormProps {
+  initialData?: BillBoard | null;
+}
+
+type BillboardFormValues = z.infer<typeof formSchema>;
+
+const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const params = useParams();
+  const router = useRouter();
+  const origin = useOrigin();
+
+  const title = initialData ? "Edit Billboard" : "Create Billboard";
+
+  const description = initialData
+    ? "Edit existing Billboard"
+    : "Create a new Billboard";
+
+  const toastMessage = initialData
+    ? "Billboard Update successfully"
+    : "Billboard create successfully";
+
+  const actionMessage = initialData ? "Save Changes" : "Add New Billboard";
+
+  const form = useForm<BillboardFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      label: "",
+      imageUrl: "",
+    },
+  });
+
+  const onSubmit = async (data: BillboardFormValues) => {
+    try {
+      setLoading(true);
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/billboards/${params.billboardId}`,
+          data
+        );
+      } else {
+        await axios.post(`/api/${params.storeId}/billboards`, data);
+      }
+
+      router.refresh();
+      toast.success(toastMessage);
+      router.push(`/${params.storeId}/billboards`);
+    } catch (error) {
+      console.log(error);
+      toast.error("[Billboard Form]: encountered an error");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+
+      await axios.delete(
+        `/api/stores/${params.storeId}/billboards/${params.billboardId}`
+      );
+      router.refresh();
+      router.push("/");
+
+      toast.success("Billboard has been deleted");
+    } catch (error) {
+      toast.error(
+        "Make sure you remove all categories using this billboard first."
+      );
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+        <span>Billboard Form</span>
+        {initialData && (
+          <Button
+            variant={"destructive"}
+            size="icon"
+            disabled={loading}
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-x-4">
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => {
+              // console.log("Rendering FormField for imageUrl:", field);
+              return (
+                <FormItem>
+                  <FormLabel>Background Image</FormLabel>
+                  <FormControl>
+                    <ImageUpload
+                      value={field.value ? [field.value] : []}
+                      disabled={loading}
+                      onChange={(url) => {
+                        console.log(url);
+                        field.onChange(url);
+                      }}
+                      onRemove={() => {
+                        field.onChange("");
+                      }}
+                    />
+                  </FormControl>
+                </FormItem>
+              );
+            }}
+          />
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="label"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Label</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={loading}
+                      placeholder="Billboard label"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className=" my-6" type="submit">
+            {actionMessage}
+          </Button>
+        </form>
+      </Form>
+      <Separator />
+      <ApiAlert
+        title="NEXT_PUBLIC-API_URL"
+        description={`${origin}/api${params.storeId}`}
+        variant="public"
+      />
+    </>
+  );
+};
+
+export default BillboardForm;
---
+++ app/api/[storeid]/sizes/[categoryId]/route.ts
@@ -0,0 +1,139 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeid: string; categoryId: string } }
+) {
+  try {
+    const { userId } = await auth();
+
+    const body = await req.json();
+
+    const { name,billboardId } = await body;
+
+    const { categoryId } = await params;
+    const { storeid } = await params;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!billboardId) {
+      return new NextResponse("Billboard Id is required", { status: 400 });
+    }
+
+    if (!categoryId) {
+      return new NextResponse("Category id is required", { status: 400 });
+    }
+
+    if (!storeid) {
+      return new NextResponse("Store Id is required this", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: storeid,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorizd", { status: 403 });
+    }
+    console.log(name,billboardId)
+    const category = await prismadb.category.updateMany({
+      where: {
+        id: categoryId,
+        // storeid: storeid,
+      },
+      data: { name:name , billboardId },
+    });
+
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log("[CATEGORY_PATCH]", error);
+  }
+  return new NextResponse("Internal error", { status: 500 });
+}
+
+/////////// Delete function
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { storeid: string; categoryId: string } }
+) {
+  try {
+    const { userId } = await auth();
+    const { storeid, categoryId } = await params;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!storeid) {
+      return new NextResponse("Store Id is required", { status: 400 });
+    }
+    if (!categoryId) {
+      return new NextResponse("category  Id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: storeid,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorizd", { status: 403 });
+    }
+
+    const category = await prismadb.category.deleteMany({
+      where: {
+        id: categoryId,
+      },
+    });
+
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log("[CATEGORY_DELETE]", error);
+  }
+  return new NextResponse("Internal error", { status: 500 });
+}
+
+////// Get function
+
+export async function GET(
+  req: Request,
+  { params }: { params: { categoryId: string } }
+) {
+  try {
+    const { userId } = await auth();
+    const { categoryId } = await params;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!categoryId) {
+      return new NextResponse("category  Id is required", { status: 400 });
+    }
+
+    const category = await prismadb.category.findUnique({
+      where: {
+        id: categoryId,
+      },
+    });
+
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log("[CATEGORY_GET]", error);
+  }
+  return new NextResponse("Internal error", { status: 500 });
+}
