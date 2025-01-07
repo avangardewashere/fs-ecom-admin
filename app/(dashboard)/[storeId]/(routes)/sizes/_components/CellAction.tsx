35a97e6caa39e88903a2fc45653240e8cca88613
avangardewashere <avelpanaligan@gmail.com>
2025-01-08 01:38:47 +0800

    modify/sizes-cell-actions-and-tested

--- app/(dashboard)/[storeId]/(routes)/sizes/_components/CellAction.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/_components/CellAction.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BillboardColumn } from "./column";
+import {  SizesColumn } from "./column";
 import {
   DropdownMenu,
   DropdownMenuTrigger,
@@ -17,7 +17,7 @@ import axios from "axios";
 import { AlertModal } from "@/components/ui/modal/alert-modal";
 
 interface CellActionProps {
-  data: BillboardColumn;
+  data: SizesColumn;
 }
 
 const CellAction: React.FC<CellActionProps> = ({ data }) => {
@@ -31,10 +31,10 @@ const CellAction: React.FC<CellActionProps> = ({ data }) => {
       navigator.clipboard
         .writeText(id)
         .then(() => {
-          toast.success("Billboard Id Copied Successfully");
+          toast.success("Sizes Id Copied Successfully");
         })
         .catch((err) => {
-          console.error("Failed to copy BillBoard ID: ", err);
+          console.error("Failed to copy Sizes ID: ", err);
         });
     } else {
       const textarea = document.createElement("textarea");
@@ -50,10 +50,10 @@ const CellAction: React.FC<CellActionProps> = ({ data }) => {
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+      await axios.delete(`/api/${params.storeId}/sizes/${data.id}`);
       router.refresh();
       //   router.push("/");
-      toast.success("Billboard deleted :" + params.storeId);
+      toast.success("sizes deleted" );
     } catch (error) {
       toast.error("");
     } finally {
@@ -85,7 +85,7 @@ const CellAction: React.FC<CellActionProps> = ({ data }) => {
             <span>Copy ID</span>
           </DropdownMenuItem>
 
-          <DropdownMenuItem onClick={()=>router.push(`/${params.storeId}/billboards/${data.id}`)}>
+          <DropdownMenuItem onClick={()=>router.push(`/${params.storeId}/sizes/${data.id}`)}>
             <Edit className="h-4 2-4" />
             <span>Update</span>
           </DropdownMenuItem>
