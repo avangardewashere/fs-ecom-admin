80211b665b7752e9e15d03d416f06c662cd34606
avangardewashere <avelpanaligan@gmail.com>
2025-01-08 09:54:21 +0800

    add/products-components

--- components/MainNav/mainNav.tsx
+++ components/MainNav/mainNav.tsx
@@ -37,13 +37,17 @@ export function MainNav({
       label: "Categories",
       active: pathname === `${params.storeId}/categories`,
     },
+    {
+      href: `/${params.storeId}/products`,
+      label: "Products",
+      active: pathname === `${params.storeId}/products`,
+    },
 
     {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `${params.storeId}/setings`,
     },
-
   ];
 
   return (
