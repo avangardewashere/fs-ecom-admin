f6122235123235fabbb020ff39fa6f309140b951
avangardewashere <avelpanaligan@gmail.com>
2025-01-14 00:56:16 +0800

    modify/env

--- app/api/[storeid]/checkout/route.ts
+++ app/api/[storeid]/checkout/route.ts
@@ -20,7 +20,7 @@ export async function POST(
     req:Request,{params}:{params:{storeid:string}}
 ){
     const {productIds} = await req.json()
-
+    const {storeid} = await params;
     if(!productIds || productIds.length ===0) {
         return new NextResponse("Product Ids are required",{status:400})
 
@@ -38,11 +38,22 @@ export async function POST(
     const lineItems:Stripe.Checkout.SessionCreateParams.LineItem[] = []
 
     products.forEach((product)=>{
-        line_items.push({
+        lineItems.push({
             quantity:1,
             price_data:{
-                Currency:'USD',
+                currency:'PHP',
+                product_data:{
+                    name:product.name
+                },
+                unit_amount:product.price * 100
             }
         })
+    })
+
+
+    const order = await prismadb.order.create({
+        data:{
+            storeId:
+        }
     })
 }
