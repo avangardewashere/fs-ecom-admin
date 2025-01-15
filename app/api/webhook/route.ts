da2a394abc9fb3542cd69105619972fb4c25a738
avangardewashere <avelpanaligan@gmail.com>
2025-01-15 16:39:52 +0800

    fix/address

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,3 +1,35 @@
+import { stripe } from "@/lib/stripe";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
 export async function POST(req: Request) {
   const body = await req.text();
+  const signature = (headers() as any).get("Stripe-Signature") as string;
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK!
+    );
+  } catch (e: any) {
+    return new NextResponse(`Webhook Erorr: ${e.message}`, { status: 400 });
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+  const address = session?.customer_details?.address;
+
+  const addressComponents = [
+    address?.line1,
+    address?.line2,
+    address?.city,
+    address?.state,
+    address?.postal_code,
+    address?.country,
+  ];
+
+  const addressString = addressComponents.filter(c=> !==null).join();
 }
