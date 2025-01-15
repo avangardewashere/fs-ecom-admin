1676fad6cdf66bac97e4b8238ec06ad97c0483a2
avangardewashere <avelpanaligan@gmail.com>
2025-01-16 04:15:19 +0800

    add/barchart-recharts

--- components/Overview.tsx
+++ components/Overview.tsx
@@ -1,10 +1,32 @@
 "use client";
+
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 interface OverviewProps {
-  data: any;
+  data: any[];
 }
 
 const Overview: React.FC<OverviewProps> = ({ data }) => {
-  return <div></div>;
+  return (
+    <ResponsiveContainer>
+      <BarChart data={data}>
+        <YAxis
+          dataKey={"name"}
+          stroke="#8888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+        />
+        <XAxis
+          stroke="#8888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+          tickFormatter={(value) => `$${value}`}
+        />
+        <Bar dataKey={"total"} fill="#3498db" radius={[4, 4, 0, 0]} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
 };
 
 export default Overview;
