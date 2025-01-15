d4d68f7ba664c3f824131573a5ae07525c94d589
avangardewashere <avelpanaligan@gmail.com>
2025-01-15 23:15:01 +0800

    add/total-recenue-actions

---
+++ actions/GetTotalRevenue.tsx
@@ -0,0 +1,27 @@
+import prismadb from "@/lib/prismadb";
+
+export const getTotralRevenue = async (storeId: string) => {
+  const paidOrders = await prismadb.order.findMany({
+    where: {
+      isPaid: true,
+      storeId,
+    },
+    include: {
+      orderItems: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  const totalRevenue = paidOrders.reduce((total, order) => {
+    const orderTotal = order.orderItems.reduce((orderSum, Item) => {
+      return orderSum + Item.product.price;
+    }, 0);
+
+    return total + orderTotal;
+  }, 0);
+
+  return totalRevenue;
+};
