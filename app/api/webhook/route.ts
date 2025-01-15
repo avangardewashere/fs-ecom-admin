606bbedff4bdd5735046ad90b66cd9aaca7093ec
avangardewashere <avelpanaligan@gmail.com>
2025-01-15 16:51:06 +0800

    done/order

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -48,5 +48,20 @@ export async function POST(req: Request) {
         orderItems: true,
       },
     });
+
+    const productIds = order.orderItems.map((orderItem) => orderItem.productId);
+
+    await prismadb.product.updateMany({
+      where: {
+        id: {
+          in: [...productIds],
+        },
+      },
+      data: {
+        isArchived: true,
+      },
+    });
   }
+
+  return new NextResponse(null, { status: 200 });
 }
