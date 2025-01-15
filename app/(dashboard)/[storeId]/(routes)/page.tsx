7139a91860f0b4302612414767c64863a6897e29
avangardewashere <avelpanaligan@gmail.com>
2025-01-15 22:42:01 +0800

    add/shadcn-ui-card

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,4 +1,6 @@
+import { Card, CardHeader } from "@/components/ui/card";
 import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 import prismadb from "@/lib/prismadb";
 
 interface DashboardPage {
@@ -19,7 +21,13 @@ const DashboardPage: React.FC<DashboardPage> = async ({ params }) => {
   return (
     <div className="flex flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <Heading title="Dashboard" description=""/> 
+        <Heading title="Dashboard" description="Overview of your store"/>
+        <Separator />  
+        <div className="grid gap-4 grid-cols-3">
+          <Card>
+            <CardHeader></CardHeader>
+          </Card>
+        </div>
       </div>
     </div>
   );
